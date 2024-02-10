af47b617b08a41fac4ee848b55d0b799b85c84f5
Alemnew Marie <alemnewmarie461@gmail.com>
2024-02-10 21:49:22 +0300

    finalize calorie counter

--- calorie-counter/script.js
+++ calorie-counter/script.js
@@ -70,4 +70,28 @@ function calculateCalories(e) {
 
     const consumedCalories = breakfastCalories + lunchCalories + dinnerCalories + snacksCalories;
     const remainingCalories = budgetCalories - consumedCalories + exerciseCalories;
+    const surplusOrDeficit = remainingCalories < 0 ? 'Surplus' : 'Deficit';
+    output.innerHTML = `
+    <span class="${surplusOrDeficit.toLowerCase()}">${Math.abs(remainingCalories)} Calorie ${surplusOrDeficit}</span>
+    <hr>
+    <p>${budgetCalories} Calories Budgeted</p>
+    <p>${consumedCalories} Calories Consumed</p>
+    <p>${exerciseCalories} Calories Burned</p>  
+    `;
+    output.classList.remove("hide");
 }
+
+calorieCounter.addEventListener("submit", calculateCalories);
+
+function clearForm() {
+    const inputContainers = Array.from(document.querySelectorAll('.input-container'));
+
+    for (const container of inputContainers) {
+        container.innerHTML = '';
+    }
+    budgetNumberInput.value = '';
+    output.innerText = '';
+    output.classList.add("hide");
+}
+
+clearButton.addEventListener("click", clearForm);

2ce214e8d40bf5e1e7e3fdfec659e49d93d93a6d
Alemnew Marie <alemnewmarie461@gmail.com>
2024-02-24 22:25:11 +0300

    finalize todo app

--- 07-todo-app/script.js
+++ 07-todo-app/script.js
@@ -10,8 +10,69 @@ const titleInput = document.getElementById("title-input");
 const dateInput = document.getElementById("date-input");
 const descriptionInput = document.getElementById("description-input");
 
-const taskData = [];
+const taskData = JSON.parse(localStorage.getItem("data")) || [];
 let currentTask = {};
+
+const addOrUpdateTask = () => {
+    addOrUpdateTaskBtn.innerText = "Add Task";
+    const dataArrIndex = taskData.findIndex((item) => item.id === currentTask.id);
+    const taskObj = {
+        id: `${titleInput.value.toLowerCase().split(" ").join("-")}-${Date.now()}`,
+        title: titleInput.value,
+        date: dateInput.value,
+        description: descriptionInput.value,
+    };
+
+    if (dataArrIndex === -1) {
+        taskData.unshift(taskObj);
+    } else {
+        taskData[dataArrIndex] = taskObj;
+    }
+
+    localStorage.setItem("data", JSON.stringify(taskData));
+    updateTaskContainer()
+    reset()
+};
+
+const updateTaskContainer = () => {
+    tasksContainer.innerHTML = "";
+    taskData.forEach(({ id, title, date, description }) => {
+        (tasksContainer.innerHTML += `
+          <div class="task" id="${id}">
+              <p><strong>Title:</strong> ${title}</p>
+              <p><strong>Date:</strong> ${date}</p>
+              <p><strong>Description:</strong> ${description}</p>
+              <button type="button" class="btn" onclick="editTask(this)">Edit</button>
+              <button type="button" class="btn" onclick="deleteTask(this)">Delete</button> 
+          </div>
+      `)
+    })
+};
+
+const deleteTask = (buttonEl) => {
+    const dataArrIndex = taskData.findIndex(
+        (item) => item.id === buttonEl.parentElement.id
+    );
+    buttonEl.parentElement.remove();
+    taskData.splice(dataArrIndex, 1);
+    localStorage.setItem("data", JSON.stringify(taskData));
+}
+
+const editTask = (buttonEl) => {
+    const dataArrIndex = taskData.findIndex(
+        (item) => item.id === buttonEl.parentElement.id
+    );
+
+    currentTask = taskData[dataArrIndex];
+
+    titleInput.value = currentTask.title;
+    dateInput.value = currentTask.date;
+    descriptionInput.value = currentTask.description;
+
+    addOrUpdateTaskBtn.innerText = "Update Task";
+
+    taskForm.classList.toggle("hidden");
+}
 
 const reset = () => {
     titleInput.value = "";
@@ -27,12 +88,17 @@ openTaskFormBtn.addEventListener("click", () =>
 
 closeTaskFormBtn.addEventListener("click", () => {
     const formInputsContainValues = titleInput.value || dateInput.value || descriptionInput.value;
-    if (formInputsContainValues) {
+    const formInputValuesUpdated = titleInput.value !== currentTask.title || dateInput.value !== currentTask.date || descriptionInput.value !== currentTask.description;
+    if (formInputsContainValues && formInputValuesUpdated) {
         confirmCloseDialog.showModal();
     } else {
         reset();
     }
 });
+
+if (taskData.length) {
+    updateTaskContainer();
+}
 
 cancelBtn.addEventListener("click", () => confirmCloseDialog.close());
 
@@ -45,28 +111,5 @@ discardBtn.addEventListener("click", () => {
 taskForm.addEventListener("submit", (e) => {
     e.preventDefault();
 
-    const dataArrIndex = taskData.findIndex((item) => item.id === currentTask.id);
-    const taskObj = {
-        id: `${titleInput.value.toLowerCase().split(' ').join('-')}-${Date.now()}`,
-        title: titleInput.value,
-        date: dateInput.value,
-        description: descriptionInput.value,
-    };
-
-    if (dataArrIndex === -1) {
-        taskData.unshift(taskObj);
-    }
-
-    taskData.forEach(({ id, title, date, description }) => {
-        (tasksContainer.innerHTML += `
-            <div class="task" id="${id}">
-                <p><strong>Title:</strong> ${title}</p>
-                <p><strong>Date:</strong> ${date}</p>
-                <p><strong>Description:</strong> ${description}</p>
-                <button type="button" class="btn">Edit</button>
-                <button type="button" class="btn">Delete</button>
-            </div>
-        `)
-        reset();
-    })
+    addOrUpdateTask();
 });
